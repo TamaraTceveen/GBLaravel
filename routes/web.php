d72ef23918102da803d43c81ab8a47d89e595829
TamaraTceveen <toma8612@gmail.com>
2022-01-16 23:05:32 +0100

    lesson1

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/hello/{UserName}', function (string $UserName) {
+    return "Hello, " . $UserName;
+});
+
+Route::get('/about', function () {
+    return "Страница о проекте";
+});
+
+Route::get('/news', function () {
+    return "Список всех новостей";
+});
+
+Route::get('/news/{id}', function (int $id) {
+    return "Вывод новости №" . $id;
+});
